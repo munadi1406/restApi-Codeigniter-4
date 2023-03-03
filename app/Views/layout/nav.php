bbf4e8d12f89af84cd6ca94ce3834c6c79675648
Fathulah Munadi <103910616+munadi1406@users.noreply.github.com>
2023-03-03 16:49:54 +0800

    Fitur (#7)
    * add update users, log views,log activity
    
    * add views log
    
    * fix folder structure

--- app/Views/layout/nav.php
+++ app/Views/layout/nav.php
@@ -8,7 +8,7 @@
        <ul class=" navbar-right">
          <li class="nav-item dropdown open" style="padding-left: 15px;">
            <a href="javascript:;" class="user-profile dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-toggle="dropdown" aria-expanded="false">
-             <img src="<?= base_url('production/images/img.jpg') ?>" alt=""><?= session('uuid') ?>
+             <img src="<?= base_url('assets/production/images/img.jpg') ?>" alt=""><?= session('uuid') ?>
            </a>
            <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
              <a class="dropdown-item" href="javascript:;"> Profile</a>

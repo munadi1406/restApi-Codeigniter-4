b57b27eae03a98e9827b159495a41e6793ccf00a
munadi1406 <munadifathullah123@gmail.com>
2023-03-03 16:43:34 +0800

    fix folder structure

--- app/Views/layout/head.php
+++ app/Views/layout/head.php
@@ -8,23 +8,23 @@
 <title><?= 'MKDIR | '.$title ?? "MKDIR DATA CENTER" ?></title>
 
 <!-- Bootstrap -->
-<link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+<link href="../assets/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 <!-- Font Awesome -->
-<link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+<link href="../assets/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
 <!-- NProgress -->
-<link href="../vendors/nprogress/nprogress.css" rel="stylesheet">
+<link href="../assets/vendors/nprogress/nprogress.css" rel="stylesheet">
 <!-- iCheck -->
-<link href="../vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+<link href="../assets/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
 
 <!-- bootstrap-progressbar -->
-<link href="../vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
+<link href="../assets/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css" rel="stylesheet">
 <!-- JQVMap -->
-<link href="../vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+<link href="../assets/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
 <!-- bootstrap-daterangepicker -->
-<link href="../vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+<link href="../assets/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
 
 <!-- Custom Theme Style -->
-<link href="../build/css/custom.min.css" rel="stylesheet">
+<link href="../assets/build/css/custom.min.css" rel="stylesheet">
 
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.2/css/jquery.dataTables.css">
 <style>
